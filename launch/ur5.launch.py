1511e0490e861153557ce0a9917c043dccd79997
wei-hsuan-cheng <johnathancheng0125@gmail.com>
2026-02-03 23:11:55 +0800

    Update code

--- launch/ur5.launch.py
+++ launch/ur5.launch.py
@@ -165,7 +165,7 @@ def generate_launch_description():
             mpc_node,
             ros2_control_node,
             controller_sequence,
-            # command_launch,
-            tt_launch,
+            command_launch,
+            # tt_launch,
         ]
     )
